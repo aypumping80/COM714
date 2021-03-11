b0aca84bd5d238826c0a61769ad90c4955fb3232
Ayodele <5awoba17@solent.ac.uk>
2021-03-11 12:58:47 +0000

    Add class alien.py.

---
+++ life/alien.py
@@ -0,0 +1,18 @@
+from flyingsuperpower import FlyingSuperPower
+from invisibility_super_power import InvisibilitySuperPower
+from livingthing import LivingThing
+
+class Alien(LivingThing, FlyingSuperPower, InvisibilitySuperPower):
+    def __init__(self, name, powers):
+        self.SUPER_POWERS = powers
+        super(LivingThing, self).__init__(name)
+        pass
+    def fly(self, distance: float) -> None:
+        if "fly" in self.SUPER_POWERS:
+            print("i am flying")
+        else:
+            print("can't fly yet!")
+        pass
+    def turn_invisible(self):
+        pass
+    def turn_visible(self):
